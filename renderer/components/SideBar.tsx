0bca1f6eeb9171364d7a1d8c0aa4b1097cf95877
浜田　俊祐 <hamashun0820@icloud.com>
2022-11-13 09:34:56 +0900

    サイドバーの調節

--- renderer/components/SideBar.tsx
+++ renderer/components/SideBar.tsx
@@ -15,65 +15,61 @@ type Props = {
 const SideBar: React.FC<Props> = ({ directoryStructure }: Props) => {
     const [positionX, setPositionX] = useState(200);
     return (
-        <>
-            {positionX >= 50 ? (
-                <div
-                    className="border-r-2 border-solid border-r-black h-screen flex"
-                    style={{ maxWidth: '200px', width: `${positionX}px` }}
-                >
-                    <div className="w-full ">
-                        {directoryStructure.map((directory) => {
-                            return (
-                                <div key={directory.parent} className="w-full">
-                                    <div className="pl-3 pt-3 overflow-hidden flex flex-col items-center">
-                                        <ul>
-                                            <Collapse
-                                                summary={
-                                                    <li className="overflow-hidden">
-                                                        <span className="text-xl font-bold cursor-pointer select-none">
-                                                            {directory.parent}
-                                                        </span>
-                                                    </li>
-                                                }
-                                                details={
-                                                    <ul className="overflow-hidden">
-                                                        {directory.children.map((child) => {
-                                                            return (
-                                                                <li key={child} className="ml-5 my-1 cursor-pointer">
-                                                                    <span className="text-lg inline-block whitespace-nowrap">
-                                                                        {child}
-                                                                    </span>
-                                                                </li>
-                                                            );
-                                                        })}
-                                                    </ul>
-                                                }
-                                            />
-                                        </ul>
-                                    </div>
-                                </div>
-                            );
-                        })}
-                    </div>
-                    <div
-                        className="h-screen w-2 cursor-col-resize"
-                        draggable
-                        onDragCapture={(e) => {
-                            if (e.clientX !== 0) {
-                                setPositionX(e.clientX);
-                            }
-                        }}
-                    ></div>
-                </div>
-            ) : (
-                <div
-                    className="h-screen w-5 cursor-col-resize"
-                    onClick={() => {
-                        setPositionX(100);
-                    }}
-                ></div>
-            )}
-        </>
+        <div
+            className={`${positionX >= 100 ? 'border-r-4' : 'border-r-2'} border-solid border-r-gray-300 h-screen flex`}
+            style={{ maxWidth: '300px', width: `${positionX}px`, minWidth: '20px' }}
+        >
+            <div className={`w-full ${positionX >= 100 ? '' : 'opacity-0'}`}>
+                {directoryStructure.map((directory) => {
+                    return (
+                        <div key={directory.parent} className="w-full">
+                            <div className="pl-3 pt-3 overflow-hidden flex flex-col items-start">
+                                <ul>
+                                    <Collapse
+                                        summary={
+                                            <li className="overflow-hidden">
+                                                <span className="text-xl font-bold cursor-pointer select-none">
+                                                    {directory.parent}
+                                                </span>
+                                            </li>
+                                        }
+                                        details={
+                                            <ul className="overflow-hidden">
+                                                {directory.children.map((child) => {
+                                                    return (
+                                                        <li key={child} className="ml-5 my-1 cursor-pointer">
+                                                            <span className="text-lg inline-block whitespace-nowrap">
+                                                                {child}
+                                                            </span>
+                                                        </li>
+                                                    );
+                                                })}
+                                            </ul>
+                                        }
+                                    />
+                                </ul>
+                            </div>
+                        </div>
+                    );
+                })}
+            </div>
+            <div
+                className={`h-screen w-2 cursor-col-resize`}
+                style={{ minWidth: '8px' }}
+                draggable
+                onDrag={(e) => {
+                    console.log(e.clientX);
+                    console.log(positionX);
+                    if (e.clientX !== 0) {
+                        if (e.clientX <= 100) {
+                            setPositionX(20);
+                            return;
+                        }
+                        setPositionX(e.clientX);
+                    }
+                }}
+            ></div>
+        </div>
     );
 };
 
