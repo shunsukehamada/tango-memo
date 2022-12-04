d67b6992f183d4e97249524e220684f6ad0b97aa
浜田　俊祐 <hamashun0820@icloud.com>
2022-12-04 11:16:05 +0900

    自動改行の修正

--- renderer/pages/register.tsx
+++ renderer/pages/register.tsx
@@ -1,5 +1,5 @@
 import { useRouter } from 'next/router';
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState, useCallback, Ref } from 'react';
 import { AnimatePresence, motion, useAnimationControls } from 'framer-motion';
 import { useForm, SubmitHandler, Controller } from 'react-hook-form';
 import Select from 'react-select';
@@ -28,13 +28,15 @@ export type PoSs = {
     Interjection: '感動詞';
 };
 
-// FIXME: 訳の自動改行
 const Register: React.FC = () => {
     const router = useRouter();
     const [isTransitioned, setIsTransitioned] = useState(false);
     const [directoryStructureState, setDirectoryStructureState] = useState<DirectoryStructure[]>([]);
     const [info, setInfo] = useState<{ japanese: string; poss: (keyof PoSs)[] }>({ japanese: '', poss: [] });
+    const [rows, setRows] = useState<number>(1);
     const animationControl = useAnimationControls();
+    const formRef = useRef();
+    const textAreaRef = useRef<HTMLTextAreaElement>();
 
     if (typeof document !== 'undefined') {
         document.addEventListener('wheel', (e) => {
@@ -52,6 +54,7 @@ const Register: React.FC = () => {
         reset,
         setValue,
     } = useForm<Inputs>({ mode: 'all', defaultValues: { japanese: '', pos: [] } });
+    const reactHookFormJapaneseRef = register('japanese').ref;
 
     useEffect(() => {
         const getAllFolders = async () => {
@@ -77,9 +80,23 @@ const Register: React.FC = () => {
     }, [isTransitioned]);
 
     useEffect(() => {
+        setRows(1);
         setValue('japanese', info.japanese);
         setValue('pos', info.poss);
     }, [info]);
+
+    useEffect(() => {
+        if (textAreaRef.current?.value === '') {
+            setRows(1);
+            return;
+        }
+        setRows(
+            Math.max(
+                Math.floor(textAreaRef.current?.scrollHeight / 28),
+                textAreaRef.current?.value.split(/\r*\n/).length
+            )
+        );
+    }, [watch('japanese')]);
 
     const onSubmit: SubmitHandler<Inputs> = (data) => {
         global.ipcRenderer.send('register-new-word', data);
@@ -99,9 +116,6 @@ const Register: React.FC = () => {
         Interjection: '感動詞',
     };
 
-    const textareaValue = watch('japanese');
-    const rows = textareaValue?.split(/\r*\n/).length;
-
     const options = [
         ...directoryStructureState.map((directory) => {
             return {
@@ -114,7 +128,26 @@ const Register: React.FC = () => {
             };
         }),
     ];
-    const formRef = useRef();
+
+    const useCombinedRefs = <T extends any>(...refs: Array<Ref<T>>): Ref<T> =>
+        useCallback(
+            (element: T) =>
+                refs.forEach((ref) => {
+                    if (!ref) {
+                        return;
+                    }
+
+                    // Ref can have two types - a function or an object. We treat each case.
+                    if (typeof ref === 'function') {
+                        return ref(element);
+                    }
+
+                    // As per https://github.com/facebook/react/issues/13029
+                    // it should be fine to set current this way.
+                    (ref as any).current = element;
+                }),
+            refs
+        );
     return (
         <>
             <AnimatePresence mode="wait">
@@ -153,6 +186,7 @@ const Register: React.FC = () => {
                                     className="flex-1 border-b-2 border-gray-300 placeholder:font-bold placeholder:text-center focus:outline-0 focus:border-blue-500 resize-none text-center text-xl font-bold placeholder:select-none"
                                     rows={rows <= 10 ? rows : 10}
                                     placeholder={errors.japanese?.type === 'required' ? '訳は必須です' : ''}
+                                    ref={useCombinedRefs(textAreaRef, reactHookFormJapaneseRef)}
                                 ></textarea>
                             </div>
                             <div className="m-1 w-5/6 flex justify-between">
